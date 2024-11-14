63691502778de27468b2c4426204a18b81f1c7fc
Shamil Siddique <shamilsdq@live.com>
2024-11-15 03:24:50 +0530

    Render companies in home page, along with loading screen

---
+++ src/components/PageLoader.jsx
@@ -0,0 +1,7 @@
+const PageLoader = () => (
+  <div className="w-screen min-h-screen max-h-screen bg-green-500 flex justify-center items-center">
+    <div className="w-14 h-14 border-8 border-white border-b-transparent rounded-full animate-spin" />
+  </div>
+);
+
+export default PageLoader;
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,5 +1,32 @@
-const Home = ()=>{
-    return <div>Home</div>
-}
+import { useEffect, useState } from "react";
+import PageLoader from "../../components/PageLoader";
+import companiesApi from "../../apis/companies";
+import CompanyList from "./CompanyList";
 
-export default Home
+const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [companies, setCompanies] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const { data } = await companiesApi.fetchAll();
+      setCompanies(data.companies);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
+  return <CompanyList companies={companies} />;
+};
+
+export default Home;
