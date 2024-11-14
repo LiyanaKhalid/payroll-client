7389277e4b279027e90f8fd4ee80a5d87a7213d0
Shamil Siddique <shamilsdq@live.com>
2024-11-15 01:12:07 +0530

    Updated axios config to work with tokens

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -2,17 +2,40 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("access-token");
+    if (accessToken) config.headers["Authorization"] = `Bearer ${accessToken}`;
+    return config;
   },
-  (error) => {
-    // if (error.response && error.response.status === 401) {
-    //   window.location.href = "/login";
-    // }
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = localStorage.getItem("refresh-token");
+        const { data } = await axiosInstance.post("/auth/refresh", {
+          refreshToken,
+        });
+        localStorage.setItem("access-token", data.accessToken);
+        localStorage.setItem("refresh-token", data.refreshToken);
+        originalRequest.headers["Authorization"] = `Bearer ${data.accessToken}`;
+        return axiosInstance(originalRequest);
+        // eslint-disable-next-line no-unused-vars
+      } catch (_refreshError) {
+        localStorage.removeItem("access-token");
+        localStorage.removeItem("refresh-token");
+        // window.location.href = "/login";
+      }
+    }
     return Promise.reject(error);
   }
 );
