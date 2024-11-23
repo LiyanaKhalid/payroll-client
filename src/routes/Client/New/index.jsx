55d7c175a837db9ba4dc0a90f8237908076ce634
Shamil Siddique <shamilsdq@live.com>
2024-11-23 15:43:30 +0530

    Updated form submit method to create new client

--- src/routes/Client/New/index.jsx
+++ src/routes/Client/New/index.jsx
@@ -1,14 +1,22 @@
 import { Formik } from "formik";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import clientsApi from "../../../apis/clients";
 import Form from "./Form";
 import { FIELDS, SCHEMA } from "./constants";
 
 const New = () => {
   const { companyId } = useParams();
+  const navigate = useNavigate();
 
-  const onFormSubmit = (values) => {
-    console.log(companyId);
-    console.log(values);
+  const onFormSubmit = async (values) => {
+    try {
+      const payload = { ...values, company_id: companyId };
+      const { data } = await clientsApi.createOne(payload);
+      if (data.error) throw new Error(data.error);
+      navigate(`/${companyId}`);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
