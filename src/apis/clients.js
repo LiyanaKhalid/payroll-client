e9fb3d83e4925930eea0da631605c96698f1985b
Shamil Siddique <shamilsdq@live.com>
2024-11-23 15:42:46 +0530

    Added api methods to create one and fetch one in clients endpoints

--- src/apis/clients.js
+++ src/apis/clients.js
@@ -5,5 +5,9 @@ const BASE_URL = "/clients";
 const fetchAll = (companyId) =>
   axiosInstance.get(BASE_URL, { params: { companyId } });
 
-const clientsApi = { fetchAll };
+const fetchOne = (id) => axiosInstance.get(`${BASE_URL}/${id}`);
+
+const createOne = (payload) => axiosInstance.post(BASE_URL, payload);
+
+const clientsApi = { fetchAll, fetchOne, createOne };
 export default clientsApi;
