6a8a805433331e85bb8ffeaee0aa653c9931e817
Shamil Siddique <shamilsdq@live.com>
2024-11-23 17:28:16 +0530

    Updated New component to support update action as well

--- src/routes/Client/New/Form.jsx
+++ src/routes/Client/New/Form.jsx
@@ -34,32 +34,29 @@ const Form = () => {
 
   return (
     <FormikForm>
-      <div className="min-w-screen min-h-screen bg-green-500 flex flex-col justify-center items-center">
-        <h2 className="text-2xl font-semibold mb-5">Add Client Details</h2>
-        <div className="bg-white p-10 px-15 rounded-lg space-y-7">
-          <div className="grid grid-cols-[300px_300px] gap-x-7 gap-y-4">
-            {FIELDS.map((item) => {
-              const InputFieldComponent = inputTypeMap[item.type] || null;
-              return (
-                <div key={item.name} className="w-full flex flex-col gap-1">
-                  <label htmlFor={item.name}>{item.label}</label>
-                  {InputFieldComponent && <InputFieldComponent data={item} />}
-                  <span className="text-red-400 text-xs">
-                    <ErrorMessage name={item.name} />
-                  </span>
-                </div>
-              );
-            })}
-          </div>
+      <div className="bg-white p-10 px-15 rounded-lg space-y-7">
+        <div className="grid grid-cols-[300px_300px] gap-x-7 gap-y-4">
+          {FIELDS.map((item) => {
+            const InputFieldComponent = inputTypeMap[item.type] || null;
+            return (
+              <div key={item.name} className="w-full flex flex-col gap-1">
+                <label htmlFor={item.name}>{item.label}</label>
+                {InputFieldComponent && <InputFieldComponent data={item} />}
+                <span className="text-red-400 text-xs">
+                  <ErrorMessage name={item.name} />
+                </span>
+              </div>
+            );
+          })}
+        </div>
 
-          <div className="w-full flex justify-end border-t">
-            <button
-              type="submit"
-              className="bg-green-500 rounded-md px-5 py-1.5 mt-5"
-            >
-              Save
-            </button>
-          </div>
+        <div className="w-full flex justify-end border-t">
+          <button
+            type="submit"
+            className="bg-green-500 rounded-md px-5 py-1.5 mt-5"
+          >
+            Save
+          </button>
         </div>
       </div>
     </FormikForm>
--- src/routes/Client/New/index.jsx
+++ src/routes/Client/New/index.jsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from "react";
 import { Formik } from "formik";
 import { useNavigate, useParams } from "react-router-dom";
 import clientsApi from "../../../apis/clients";
+import PageLoader from "../../../components/PageLoader";
 import Form from "./Form";
 import { FIELDS, SCHEMA } from "./constants";
 
 const New = () => {
-  const { companyId } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [client, setClient] = useState();
+
+  // clientId is available if in edit path, else will be undefined
+  // So, clientId having value means edit form, else add form
+  const { companyId, clientId } = useParams();
   const navigate = useNavigate();
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await clientsApi.fetchOne(clientId);
+      setClient(data.client);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // TODO: Support update submit also
   const onFormSubmit = async (values) => {
     try {
       const payload = { ...values, company_id: companyId };
-      const { data } = await clientsApi.createOne(payload);
+      const { data } = client?.id
+        ? await clientsApi.createOne(payload)
+        : await clientsApi.updateOne(client.id, payload);
       if (data.error) throw new Error(data.error);
       navigate(`/${companyId}`);
     } catch (err) {
@@ -19,20 +41,35 @@ const New = () => {
     }
   };
 
+  const getInitialFormValues = () => {
+    return FIELDS.reduce(
+      (values, field) => ({
+        ...values,
+        [field.name]: client?.[field.name] ?? "",
+      }),
+      {}
+    );
+  };
+
+  useEffect(() => {
+    if (clientId) fetchData();
+  }, [clientId]);
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
   return (
-    <Formik
-      initialValues={FIELDS.reduce(
-        (values, field) => ({
-          ...values,
-          [field.name]: "",
-        }),
-        {}
-      )}
-      validationSchema={SCHEMA}
-      onSubmit={onFormSubmit}
-    >
-      {() => <Form />}
-    </Formik>
+    <div className="min-w-screen min-h-screen bg-green-500 flex flex-col justify-center items-center">
+      <h2 className="text-2xl font-semibold mb-5">Add Client Details</h2>
+      <Formik
+        initialValues={getInitialFormValues()}
+        validationSchema={SCHEMA}
+        onSubmit={onFormSubmit}
+      >
+        {() => <Form />}
+      </Formik>
+    </div>
   );
 };
 
