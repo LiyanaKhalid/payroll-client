ff9003801793aa94526db23f8367a1395ffb44c8
Shamil Siddique <shamilsdq@live.com>
2024-11-29 02:21:24 +0530

    Added API methods for employees

---
+++ src/apis/employees.js
@@ -0,0 +1,13 @@
+import axiosInstance from "../configs/axios";
+
+const BASE_URL = "/employees";
+
+const fetchOne = (id) => axiosInstance.get(`${BASE_URL}/${id}`);
+
+const createOne = (payload) => axiosInstance.post(BASE_URL, payload);
+
+const updateOne = (id, payload) =>
+  axiosInstance.put(`${BASE_URL}/${id}`, payload);
+
+const employeesApi = { fetchOne, createOne, updateOne };
+export default employeesApi;
